6f36ca672c509bf46649ba7d9e40b37bb3905ed3
ikramebakkari <ikrame.bakkari@insa.ueuromed.org>
2023-11-28 15:56:30 +0100

    clazz and classname with methods and and attributes display using a hashtable

--- src/ClassAnalyzer.java
+++ src/ClassAnalyzer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Hashtable;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.logging.Level;
@@ -6,6 +7,7 @@ import java.util.logging.Logger;
 
 public class ClassAnalyzer {
     private static final Logger logger = Logger.getLogger(ClassAnalyzer.class.getName());
+    private final Hashtable<String, ClassInfo> classInfoTable = new Hashtable<>();
 
     public void analyzeClasses(String jarFilePath) {
         try (JarFile jarFile = new JarFile(jarFilePath)) {
@@ -15,6 +17,12 @@ public class ClassAnalyzer {
                     .count();
 
             logger.info("Total Classes: " + totalClasses);
+
+            // Display the stored data in CSV format
+            logger.info("Class Name,Class Object,Method Count,Attribute Count");
+            for (ClassInfo classInfo : classInfoTable.values()) {
+                logger.info(classInfo.toCsvString());
+            }
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error analyzing classes in JAR file", e);
         }
@@ -24,14 +32,43 @@ public class ClassAnalyzer {
         String className = entry.getName().replace("/", ".").replace(".class", "");
 
         // Extract class name from the entry
-        logger.info("Class: " + className);
+        logger.info("Analyzing class: " + className);
 
         // Use a ClassReader or other means to further analyze the class file and extract methods and attributes
-        // For simplicity, we'll just print a placeholder count here
+        // For simplicity, we'll just use placeholder counts here
         long methodCount = 10; // Replace with actual count
         long attributeCount = 5; // Replace with actual count
 
-        logger.info("  Methods: " + methodCount);
-        logger.info("  Attributes: " + attributeCount);
+        // Load the class
+        Class<?> clazz = null;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            logger.log(Level.SEVERE, "Error loading class: " + className, e);
+        }
+
+        // Store the information in the table
+        classInfoTable.put(className, new ClassInfo(className, clazz, methodCount, attributeCount));
+    }
+
+    // A simple class to store information about a class
+    private static class ClassInfo {
+    private static class ClassInfo {
+        private final String className;
+        private final Class<?> clazz;
+        private final long methodCount;
+        private final long attributeCount;
+
+        public ClassInfo(String className, Class<?> clazz, long methodCount, long attributeCount) {
+            this.className = className;
+            this.clazz = clazz;
+            this.methodCount = methodCount;
+            this.attributeCount = attributeCount;
+        }
+
+        public String toCsvString() {
+            return String.format("%s,%s,%d,%d",
+                    className, clazz, methodCount, attributeCount);
+        }
     }
 }
